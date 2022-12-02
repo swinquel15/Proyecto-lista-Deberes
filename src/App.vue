0a578c7590163a949cd202bc52a4c7c5523d0046
Carlos Pelinco <carlos.pelinco.h@uni.pe>
2022-12-01 23:07:10 -0500

    se agrego componentes en vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="header"></div>
+    <div id="main-container">
+      <h2>Todos</h2>
+      <TodosWeb v-bind:todoslist="copyTodosWeb"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+//import SearchWeb from './components/SearchWeb'
+import TodosWeb from './components/TodosWeb';
+//import TodoAdd from './components/TodoAdd'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodosWeb
+  },
+  data(){
+    return{
+      todos:[
+        {
+          id: 0,
+          title:'comprar la cena',
+          completed:false
+        },
+        {
+          id: 1,
+          title:'Sacar a pasear al perro',
+          completed:true
+        },
+        {
+          id:2,
+          title:'jugar Xbox',
+          completed:false
+        }
+      ],
+      copyTodosWeb:[]
+    }
+  },
+  created(){
+    this.copyTodosWeb=[...this.todos];
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;
+  }
+  #main-container{
+    border: solid 1px #ccc;
+    width: 600px;
+    margin: 100px auto;
+  }
+  #header{
+    background: black;
+    padding: 10px;
+  }
+  h2{
+    padding: 0 10px;
+  }
 </style>
---
+++ src/components/TodosWeb.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todoslist">
+            <div>{{todo.title}}</div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name:'TodosWeb',
+        props:['todoslist']
+    }
+
+</script>
+
+
+<style></style>
